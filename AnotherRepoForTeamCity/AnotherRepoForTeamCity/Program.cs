9cbb2e84865a3efc1480c393cd0054d11f7f11b0
NickoVin <blackmarkerq@gmail.com>
2022-03-27 15:21:05 +0300

    error

--- AnotherRepoForTeamCity/AnotherRepoForTeamCity/Program.cs
+++ AnotherRepoForTeamCity/AnotherRepoForTeamCity/Program.cs
@@ -6,7 +6,7 @@ namespace AnotherRepoForTeamCity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
         }
     }
 }
